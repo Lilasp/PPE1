05a963464c70d75be7584ec1120fbeb780b9e9c7
Lpastre <lilasp@PC-Lilas.localdomain>
2022-10-19 09:21:41 +0200

    Ajout des conditions

--- premier_script.sh
+++ premier_script.sh
@@ -1,1 +1,13 @@
 echo où suis-je ?
+
+if [ 0 = 0 ] #un seul "=" même si condition
+# -z $1 --> vérifie que l'argument 1 n'a pas été donné
+#-n $1 --> vérifie que l'argument 1 a été donné
+
+then 
+#NE PAS OUBLIER LE THEN
+	echo "la "
+else 
+	echo "le "
+fi
+#NE PAS OUBLIER LE FI
